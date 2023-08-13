6d251803729b9d7c7698f0a37f4c897ef7efe07d
Strike24 <benxcohen@gmail.com>
2023-08-13 12:40:18 +0300

    Redone the entire system, added censoring for Edit Server

--- src/main/java/me/kokostrike/creatortools/managers/StreamerModeManager.java
+++ src/main/java/me/kokostrike/creatortools/managers/StreamerModeManager.java
@@ -2,27 +2,33 @@ package me.kokostrike.creatortools.managers;
 
 import me.kokostrike.creatortools.config.ConfigSettings;
 import me.kokostrike.creatortools.config.ConfigSettingsProvider;
+import me.kokostrike.creatortools.menus.DirectConnectCensored;
+import me.kokostrike.creatortools.menus.MultiplayerScreenCensored;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.AddServerScreen;
+import net.minecraft.client.gui.screen.DirectConnectScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 
 import java.util.Objects;
 
 public class StreamerModeManager {
     private final ConfigSettings configSettings;
-    private String lastServerBeforeStreamerMode = "";
     public StreamerModeManager() {
         this.configSettings = ConfigSettingsProvider.getConfigSettings();
 
         ClientTickEvents.START_CLIENT_TICK.register((listener) -> {
-            if (listener.currentScreen instanceof MultiplayerScreen) {
-                if (!listener.options.lastServer.equals("Streamer Mode is enabled!") && configSettings.isStreamerMode()) {
-                    lastServerBeforeStreamerMode = listener.options.lastServer;
-                    listener.options.lastServer = "Streamer Mode is enabled!";
-                } else if (!listener.options.lastServer.equals(lastServerBeforeStreamerMode) && !configSettings.isStreamerMode()) {
-                    listener.options.lastServer = lastServerBeforeStreamerMode;
-                }
+            if (!configSettings.isStreamerMode()) return;
+
+            if (!(listener.currentScreen instanceof MultiplayerScreenCensored) && listener.currentScreen instanceof MultiplayerScreen realScreen ) {
+                listener.setScreen(new MultiplayerScreenCensored(realScreen));
             }
+//
+//            if (!(listener.currentScreen instanceof DirectConnectCensored) && listener.currentScreen instanceof DirectConnectScreen realScreen) {
+//                listener.setScreen(new DirectConnectCensored(realScreen));
+//                MinecraftClient.getInstance().options.lastServer = lastServer;
+//            }
+
         });
     }
 }
